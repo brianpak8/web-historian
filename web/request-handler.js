ad0963caf6b976304dcf10004b617c0342c12b66
Hack Reactor Student <student@hackreactor.com>
2017-11-01 15:36:25 -0700

    serving up static html file

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,37 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers.js');
 // require more modules/folders here!
 
+
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  if (req.method === 'GET') {
+    var indexPath = archive.paths.siteAssets.concat('/index.html');
+
+    httpHelpers.serveAssets(res, indexPath, function(err, data) {
+      if (err) {
+        res.statusCode = 500;
+        res.end(`Error getting the file ${indexPath}`);
+      } else {
+        res.writeHead(200, httpHelpers.headers);
+        res.end(data);
+      }
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+//  USE BELOW LATER
+//res.end(archive.paths.list);
 };
